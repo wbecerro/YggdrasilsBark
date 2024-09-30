e8a2a9f424984786e2209f20675255a68053165b
wbe <34304532+wbecerro@users.noreply.github.com>
2024-10-01 01:05:47 +0200

    Arreglado un bug con el nuevo evento.

--- src/main/java/wbe/yggdrasilsBark/events/PlayerReceiveRewardEvent.java
+++ src/main/java/wbe/yggdrasilsBark/events/PlayerReceiveRewardEvent.java
@@ -1,21 +1,32 @@
 package wbe.yggdrasilsBark.events;
 
 import org.bukkit.entity.Player;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
-import org.bukkit.event.player.PlayerEvent;
 import wbe.yggdrasilsBark.rarities.Rarity;
 import wbe.yggdrasilsBark.rarities.Reward;
 
-public class PlayerReceiveRewardEvent extends PlayerEvent {
+public class PlayerReceiveRewardEvent extends Event implements Cancellable {
+
+    private Player player;
 
     private Rarity rarity;
 
     private Reward reward;
 
+    private boolean isCancelled;
+
+    private static final HandlerList handlers = new HandlerList();
+
     public PlayerReceiveRewardEvent(Player player, Rarity rarity, Reward reward) {
-        super(player);
+        this.player = player;
         this.rarity = rarity;
         this.reward = reward;
+    }
+
+    public Player getPlayer() {
+        return player;
     }
 
     public Rarity getRarity() {
@@ -26,8 +37,22 @@ public class PlayerReceiveRewardEvent extends PlayerEvent {
         return reward;
     }
 
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
+
     @Override
     public HandlerList getHandlers() {
-        return null;
+        return handlers;
+    }
+
+    @Override
+    public boolean isCancelled() {
+        return isCancelled;
+    }
+
+    @Override
+    public void setCancelled(boolean isCancelled) {
+        this.isCancelled = isCancelled;
     }
 }
